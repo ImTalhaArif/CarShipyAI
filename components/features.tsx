6f518ac8e826f0b1f753ac36edc1f455a73e7b80
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2023-05-17 20:35:29 +0500

    Add files via upload

---
+++ components/features.tsx
@@ -0,0 +1,72 @@
+export default function Features() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Our team has years of experience in the industry, with expertise ranging from vehicle transport logistics to international and individualized service. </h2>
+            <p className="text-xl text-gray-400"> This means you can depend on us to provide the highest level of service, support, and customer satisfaction.</p>
+          </div>
+
+          {/* Items */}
+          <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg>
+              <h4 className="h4 mb-2">Our carriers are fully insured, licensed,</h4>
+              <p className="text-lg text-gray-400 text-center">and provide detailed tracking updates so that you always know where your vehicle is located. Our carriers ensure all vehicles are properly loaded and secured before they’re transported.</p>
+            </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="100" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <circle className="fill-current text-purple-600" cx="32" cy="32" r="32" />
+                <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
+              </svg>
+              <h4 className="h4 mb-2">At Carshipy,</h4>
+              <p className="text-lg text-gray-400 text-center">we are committed to providing quality auto transport lead services that increase your efficiency, productivity, and profitability. </p>
+            </div>
+
+            {/* 3rd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <g transform="translate(21 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <ellipse className="stroke-current text-purple-300" cx="11" cy="11" rx="5.5" ry="11" />
+                  <path className="stroke-current text-purple-100" d="M11 0v22M0 11h22" />
+                  <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Our goal is to provide high-level service</h4>
+              <p className="text-lg text-gray-400 text-center">at an affordable cost for our valued clientele base, without compromising quality.  Our carriers use only the highest quality materials and equipment when handling your vehicle’s transport.This gives our customers peace of mind and ensures their vehicles are transported in a secure manner.</p>
+            </div>
+
+            {/* 4th item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="300" data-aos-anchor="[data-aos-id-blocks]">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <g transform="translate(22 21)" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd">
+                  <path className="stroke-current text-purple-100" d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5" />
+                  <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">We invite you to experience the Carshipy difference today.</h4>
+              <p className="text-lg text-gray-400 text-center"> With years of experience and a commitment to excellence, you can rest assured that your car will arrive safe, sound, and on time. Contact us today to find out how we can help you with your automobile transport needs.</p>
+            </div>
+
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
